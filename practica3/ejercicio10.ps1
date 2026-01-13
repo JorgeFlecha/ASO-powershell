b4558ad75d16db1319f10b85e11f593366f61c65
Jorge Flecha López <133894257+JorgeFlecha@users.noreply.github.com>
2026-01-13 20:22:26 +0100

    Add files via upload

---
+++ practica3/ejercicio10.ps1
@@ -0,0 +1,21 @@
+﻿$puntuacion = [double](Read-Host "Introduce la puntuación obtenida")
+$dinero = 2400
+
+if ($puntuacion -eq 0.0) {
+    $nivel = "Inaceptable"
+}
+elseif ($puntuacion -eq 0.4) {
+    $nivel = "Aceptable"
+}
+elseif ($puntuacion -ge 0.6) {
+    $nivel = "Meritorio"
+}
+else {
+    Write-Host "La puntuación no es válida, tiene que ser 0.0, 0.4 o 0.6 o más."
+    exit
+}
+
+$dinero = $puntuacion * $dinero
+
+Write-Host "Nivel: $nivel"
+Write-Host "Has conseguido: $dinero €"
---
+++ practica3/ejercicio11.ps1
@@ -0,0 +1,11 @@
+﻿$edad = [int](read-host "Introduce tu edad")
+
+if ($edad -lt 4) {
+    write-host "La entrada es gratis."
+}
+elseif ($edad -ge 4 -and $edad -le 18) {
+    write-host = "La entrada es de 5€"
+}
+elseif ($edad -gt 18) {
+    write-host = "La entrada es de 10€"
+}

d62aa037d990b298b7ce7366dd784ae57247ec25
Jorge Flecha López <133894257+JorgeFlecha@users.noreply.github.com>
2026-01-30 13:47:40 +0100

    Create ejercicio.ps1

---
+++ unidad7/practica2/ejercicio.ps1
@@ -0,0 +1,25 @@
+New-Item -ItemType Directory -Path "C:\" -Name "Empresa"
+#permisos carpeta empresa y heredados
+$carpeta = "C:\Empresa"
+$acl_carpeta = Get-acl $carpeta
+$acl_carpeta.SetAccessRuleProtection($true, $false)
+$permisoadd = @('Administradores', 'FullControl', 'ContainerInherit, ObjectInherit', 'None', 'Allow')
+$permisoadd2 = @('Usuarios del dominio', 'Read', 'ContainerInherit, ObjectInherit', 'None', 'Allow')
+$ace= New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisoadd
+$ace2= New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisoadd2
+
+$acl_carpeta.AddAccessRule($ace)
+$acl_carpeta.AddAccessRule($ace2)
+$acl_carpeta | Set-Acl -Path $carpeta
+
+#permisos departamentos
+Import-Csv "C:\Users\Administrador\Downloads\departamentos.csv" -Delimiter ";" | foreach {
+    New-Item -ItemType Directory -Path "C:\Empresa" -Name $_.departamento
+    $nombre = $_.departamento
+    $departamento = "C:\Empresa\$($nombre)"
+    $acl_departamentos = Get-acl $departamento
+    $permisoadd_departamento = @("$nombre", 'Modify', 'ContainerInherit, ObjectInherit', 'None', 'Allow')
+    $ace3= New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisoadd_departamento
+    $acl_departamentos.SetAccessRule($ace3)
+    $acl_departamentos | Set-Acl -Path $departamento
+}

cd3d77c1002b2ae560013ca2fcd1949f76f1183d
Jorge Flecha López <133894257+JorgeFlecha@users.noreply.github.com>
2026-02-06 12:50:46 +0100

    Update ejercicio.ps1

--- unidad7/practica4/ejercicio.ps1
+++ unidad7/practica4/ejercicio.ps1
@@ -1,6 +1,6 @@
 Import-Csv "C:\Users\Administrador\Downloads\empleados.csv" -Delimiter ";" | Foreach-object {
     $usuario = "$($_.nombre).$($_.apellido)"
     if ($_.departamento -eq "PERSONAL") {
-        Set-ADUser -Identity $usuario -ProfilePath \\empresa-jorge\Empresa_users$\$usuario
+        Set-ADUser -Identity $usuario -ProfilePath \\empresa-jorge\Empresa_users$\$usuario\
     }
 }
